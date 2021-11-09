73bbbe4a31b4724aba9a19dbe7194e01fe423cab
Arsh <87128542+ArshCypherZ@users.noreply.github.com>
2021-11-09 13:56:26 +0530

    Update telegraph.py

--- wbb/modules/telegraph.py
+++ wbb/modules/telegraph.py
@@ -4,24 +4,162 @@ from pyrogram.types import Message
 from wbb import app, telegraph
 from wbb.core.decorators.errors import capture_err
 
-__MODULE__ = "Telegraph"
-__HELP__ = "/telegraph [Page name]: Paste styled text on telegraph."
+from wbb.tevents import register
 
+from wbb import telethn as tbot
 
-@app.on_message(filters.command("telegraph"))
-@capture_err
-async def paste(_, message: Message):
-    reply = message.reply_to_message
+TMP_DOWNLOAD_DIRECTORY = "./"
 
-    if not reply or not reply.text:
-        return await message.reply("Reply to a text message")
+from telethon import events
 
-    if len(message.command) < 2:
-        return await message.reply("**Usage:**\n /telegraph [Page name]")
+import os
 
-    page_name = message.text.split(None, 1)[1]
-    page = telegraph.create_page(page_name, html_content=(reply.text.html).replace("\n", "<br>"))
-    return await message.reply(
-        f"**Posted:** {page['url']}",
-        disable_web_page_preview=True,
-    )
+from PIL import Image
+
+from datetime import datetime
+
+from telegraph import Telegraph, upload_file, exceptions
+
+emilia = "EMILIA"
+
+telegraph = Telegraph()
+
+r = telegraph.create_account(short_name=emilia)
+
+auth_url = r["auth_url"]
+
+@register(pattern="^/t(m|xt) ?(.*)")
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    optional_title = event.pattern_match.group(2)
+
+    if event.reply_to_msg_id:
+
+        start = datetime.now()
+
+        r_message = await event.get_reply_message()
+
+        input_str = event.pattern_match.group(1)
+
+        if input_str == "m":
+
+            downloaded_file_name = await tbot.download_media(
+
+                r_message,
+
+                TMP_DOWNLOAD_DIRECTORY
+
+            )
+
+            end = datetime.now()
+
+            ms = (end - start).seconds
+
+            h = await event.reply("Downloaded {} in {} seconds.".format(downloaded_file_name, ms))
+
+            if downloaded_file_name.endswith((".webp")):
+
+                resize_image(downloaded_file_name)
+
+            try:
+
+                start = datetime.now()
+
+                media_urls = upload_file(downloaded_file_name)
+
+            except exceptions.TelegraphException as exc:
+
+                await h.edit("ERROR: " + str(exc))
+
+                os.remove(downloaded_file_name)
+
+            else:
+
+                end = datetime.now()
+
+                ms_two = (end - start).seconds
+
+                os.remove(downloaded_file_name)
+
+                await h.edit("Uploaded to [Telegraph](https://telegra.ph{})".format(media_urls[0]), link_preview=True)
+
+        elif input_str == "xt":
+
+            user_object = await tbot.get_entity(r_message.sender_id)
+
+            title_of_page = user_object.first_name # + " " + user_object.last_name
+
+            # apparently, all Users do not have last_name field
+
+            if optional_title:
+
+                title_of_page = optional_title
+
+            page_content = r_message.message
+
+            if r_message.media:
+
+                if page_content != "":
+
+                    title_of_page = page_content
+
+                downloaded_file_name = await tbot.download_media(
+
+                    r_message,
+
+                    TMP_DOWNLOAD_DIRECTORY
+
+                )
+
+                m_list = None
+
+                with open(downloaded_file_name, "rb") as fd:
+
+                    m_list = fd.readlines()
+
+                for m in m_list:
+
+                    page_content += m.decode("UTF-8") + "\n"
+
+                os.remove(downloaded_file_name)
+
+            page_content = page_content.replace("\n", "<br>")
+
+            response = telegraph.create_page(
+
+                title_of_page,
+
+                html_content=page_content
+
+            )
+
+            end = datetime.now()
+
+            ms = (end - start).seconds
+
+            await event.reply("Pasted to [Telegraph](https://telegra.ph/{}) in {} seconds.".format(response["path"], ms), link_preview=True)
+
+    else:
+
+        await event.reply("Reply to a message to get a permanent telegra.ph link.")
+
+def resize_image(image):
+
+    im = Image.open(image)
+
+    im.save(image, "PNG")
+
+__help__ = """
+
+ ❍ /tm :Get Telegraph Link Of Replied Media
+
+ ❍ /txt :Get Telegraph Link of Replied Text
+
+"""
+
+__mod_name__ = "Telegraph"
